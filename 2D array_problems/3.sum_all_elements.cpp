f555dd4fd4e2cafb1c8c0019d269e23fa4419201
Adarsh shukla <adarshbhai917963@gmail.com>
2026-04-30 22:57:18 +0530

    Add files via upload

---
+++ 2D array_problems/3.sum_all_elements.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n,m;
+    cout<<"Enter the number of row : ";
+    cin>>n;
+
+    cout<<"Enter the value of column : ";
+    cin>>m;
+
+    int arr[n][m];
+
+    // Take Input 
+
+    cout<<"Enter the value of matrix \n";
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            cout<<arr[i][j]<<" | ";
+        }
+        cout<<"\n";
+    }
+
+    int sum = 0;
+    
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            sum += arr[i][j];
+        }
+        cout<<"\n";
+    }
+
+    cout<<"Sum : "<<sum;
+
+}
