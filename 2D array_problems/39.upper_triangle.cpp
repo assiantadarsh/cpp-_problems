9239388f21ee5d4c95f25fbf1a70eaf13a551743
Adarsh shukla <adarshbhai917963@gmail.com>
2026-05-16 20:55:17 +0530

    Add files via upload

---
+++ 2D array_problems/39.upper_triangle.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+// Que -> Print all upper triangular elements.
+
+int main(){
+
+    int n,m;
+    cout<<"Enter the number of row : ";
+    cin>>n;
+
+    cout<<"Enter the value of column : ";
+    cin>>m;
+    
+    if(n <= 0 || m <= 0){
+        cout<<"Can't possible";
+        return 0;
+    }
+
+    if(n != m){
+        cout<<"Can't Possible";
+        return 0;
+    }
+
+    int arr[n][m];
+
+    // Take Input 
+
+    cout<<"Enter the value of matrix \n";
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            cout<<arr[i][j]<<" | ";
+        }
+        cout<<"\n";
+    }
+    cout<<"Upper Triangle : \n";
+    
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            if(i <= j){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+
+}
