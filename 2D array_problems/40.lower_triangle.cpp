bb2d1731472db97b6fb54449403c044358a63f7c
Adarsh shukla <adarshbhai917963@gmail.com>
2026-05-16 20:57:56 +0530

    Add files via upload

---
+++ 2D array_problems/40.lower_triangle.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+// Que -> Print all lower triangular elements.
+
+int main(){
+
+    int n,m;
+    cout<<"Enter the number of row : ";
+    cin>>n;
+
+    cout<<"Enter the value of column : ";
+    cin>>m;
+    
+    if(n <= 0 || m <= 0){
+        cout<<"Can't possible";
+        return 0;
+    }
+
+    int arr[n][m];
+
+    // Take Input 
+
+    cout<<"Enter the value of matrix \n";
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            cout<<arr[i][j]<<" | ";
+        }
+        cout<<"\n";
+    }
+    
+    cout<<"Lower Triangular \n";
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < m ; j++){
+            if(i >= j){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+
+
+}
