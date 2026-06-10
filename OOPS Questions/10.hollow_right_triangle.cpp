ada470dba7fa388d10a9c3b3bf035844c9f11859
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-11 01:42:23 +0530

    Add files via upload

---
+++ OOPS Questions/10.hollow_right_triangle.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+// *
+// * *
+// *   *
+// *     *
+// * * * * * 
+
+class pattern{
+
+    private:
+    int n;
+
+    public:
+    pattern(int size){
+        n = size;
+    }
+
+    void display(){
+
+        for(int i = 1;i < n;i++){
+
+            cout<<"*";
+            // spaces
+
+            for(int j = 1;j <= 2 * i -3;j++){
+                cout<<" ";
+            }
+            if(i > 1){
+                cout<<"*";
+            }
+
+            cout<<"\n";
+        }
+
+        // Last line
+        for(int k = 1;k <= n;k++){
+            cout<<"*"<<" ";
+        }
+
+    }
+
+};
+
+int main(){
+
+    int size;
+    cout<<"Enter the positive value of size : ";
+    cin>>size;
+
+    pattern p(size);
+    p.display();
+    
+}
