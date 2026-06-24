25ffa9115323707a0d0dad4844f1f1ad6212d17d
Adarsh shukla <adarshbhai917963@gmail.com>
2026-06-24 22:42:37 +0530

    Create star_pyramid.cpp

---
+++ OOPS Questions/Encapsulation_problem/star_pyramid.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+/*
+Create a class Pattern using encapsulation.
+Keep n private.
+Print simple star pyramid pattern.
+
+Example for n = 5:
+
+    *
+   ***
+  *****
+ *******
+*********
+*/
+
+class Pattern {
+private:
+    int n;
+
+public:
+    void input() {
+        cout << "Enter size: ";
+        cin >> n;
+    }
+
+    void display() {
+        for(int i = 1; i <= n; i++) {
+
+            // spaces
+            for(int j = 1; j <= n - i; j++) {
+                cout << " ";
+            }
+
+            // stars
+            for(int j = 1; j <= 2 * i - 1; j++) {
+                cout << "*";
+            }
+
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Pattern p;
+
+    p.input();
+    p.display();
+
+    return 0;
+}
